168e84d764a37a17ae54ccf507a0398c1e665d6b
Noco117 <nicolai.prodanov@gmail.com>
2024-06-24 15:43:17 +0200

    Added Node and Maze struct

--- src/core.jl
+++ src/core.jl
@@ -1,5 +1,24 @@
 mutable struct Node{}
+    key::Int
+
+    up::Union{Node, Nothing}
+    down::Union{Node, Nothing}
+    left::Union{Node, Nothing}
+    right::Union{Node, Nothing}
+end
+
+function neighbors(node::Node)::Vector{Node}
+    temp::Vector{Node} = []
+    for neighbor in [node.up, node.down, node.left, node.right]
+        if !isnothing(neighbor)
+            push!(temp, neighbor)
+        end
+    end
+    return temp
 end
 
 mutable struct Maze{}
+    nodes::Matrix{Node}
+    # visual::Union
+    path::Union{Vector{Node}, Nothing}
 end
