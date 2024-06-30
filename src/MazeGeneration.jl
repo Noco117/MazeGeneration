0bdcb63c27cb8e1bdfb2982ac8f6b6e2e14fd012
Noco117 <nicolai.prodanov@gmail.com>
2024-06-30 20:59:10 +0200

    Fixed maze Constructor for randomized Mazes, restructured core import

--- src/MazeGeneration.jl
+++ src/MazeGeneration.jl
@@ -1,23 +1,7 @@
 module MazeGeneration
 include("core.jl")
+using .Core
+
 include("randomizedMaze.jl")
-
-# nodes = [Node(0, nothing, nothing, nothing, nothing) Node(0, nothing, nothing, nothing, nothing)
-#          Node(0, nothing, nothing, nothing, nothing) Node(0, nothing, nothing, nothing, nothing)]
-
-# nodes[1, 1].right = nodes[1, 2]
-# nodes[1, 2].left = nodes[1, 1]
-# nodes[1, 2].down = nodes[2, 2]
-# nodes[2, 2].up = nodes[1, 2]
-# nodes[2, 2].left = nodes[2, 1]
-# nodes[2, 1].right = nodes[2, 2]
-
-# function Base.show(io::IO, node::Node) 
-#     println(io, [!isnothing(node.left), !isnothing(node.right), !isnothing(node.up), !isnothing(node.down)])
-# end
-
-# alt = maze(3, 3)
-# print(alt.nodes)
-
 end # module MazeGeneration
 
--- src/randomizedMaze.jl
+++ src/randomizedMaze.jl
@@ -1,4 +1,5 @@
-using .Core, Random
+using ..Core
+using Random
 using DataStructures
 
 # maze Konstruktor, der ein zufälliges n x m Maze per Tiefensuche generiert
@@ -11,15 +12,18 @@ function maze(height::Int, width::Int)::Maze
     i = rand(1:height)
     j = rand(1:width)
 
-    #Erstellt einen Stack für die Tiefensuche und fügt den Startknoten hinzu
-    stack = Stack{Node}()
-    push!(stack, nodes[i,j])
+    #Erstellt einen Stack für die Tiefensuche und fügt den Startknoten mit seinen Koordinaten im Maze hinzu
+    stack = Stack{Tuple{Node, Int, Int}}()
+    push!(stack, (nodes[i,j], i, j))
 
     #Führt randomisierte Tiefensuche aus also wird immer ein zufälliger unbesuchter Knoten in den Stack hinzugefügt
     while !isempty(stack)
-        node = top(stack)
+        node, i, j = top(stack)
         node.key = 1 # Markiert Knoten als besucht
-        
+
+
+        # Überprüft für alle Richtungen ob Knoten ob es benachbarte Knoten gibt, bzw ob in der Richtung ein Rand ist.
+        # Dann werden alle Richtungen in einer Liste gespeichert, für die sich der Knoten, der sich in der Richtung befindet noch unbesucht ist
         adjacent_dirs = [x for x in [[-1, 0], [1, 0], [0, -1], [0, 1]] if 1 <= x[1] + i && x[1] + i <= height && 1 <= x[2] + j && x[2] + j <= width]
         unvisited_adjacent_dirs = [coord for coord in adjacent_dirs if nodes[i + coord[1], j + coord[2]].key == 0]
         
@@ -28,11 +32,13 @@ function maze(height::Int, width::Int)::Maze
             continue
         end
 
+
         direction = rand(unvisited_adjacent_dirs)
-        
-        k, l = i+direction[1], j+direction[2]
+
+        i, j = i+direction[1], j+direction[2]
         next_node = nodes[i, j]
         
+        # Verbindet Knoten mit dem ausgewählten nächsten
         if direction == [0, -1]
             node.left = next_node
             next_node.right = node
@@ -47,8 +53,7 @@ function maze(height::Int, width::Int)::Maze
             next_node.up = node
         end
 
-        push!(stack, next_node)
-        i, j = k, l
+        push!(stack, (next_node, i, j))
     end
 
     return Maze(nodes, nothing)
