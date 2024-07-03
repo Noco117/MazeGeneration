9e25f54e09626b92ccce6fe41267e147ebf4026b
Noco117 <nicolai.prodanov@gmail.com>
2024-07-03 04:54:24 +0200

    added IO to print for show(io, node::Node)

--- src/core.jl
+++ src/core.jl
@@ -12,7 +12,7 @@ mutable struct Node
     right::Union{Node, Nothing}
 end
 
-Base.show(io::IO, node::Node) = node.key
+Base.show(io::IO, node::Node) = println(io, node.key)
 
 function neighbors(node::Node)::Vector{Node}
     temp::Vector{Node} = []
